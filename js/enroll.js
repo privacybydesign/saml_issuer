14ad6633c35801514a0becad0cda8a414417c43b
Sietse Ringers <mail@sietseringers.net>
2017-03-02 18:25:33 +0100

    Actually include javascript files

---
+++ js/enroll.js
@@ -0,0 +1,44 @@
+$(function() {
+	var iprequest = {
+		timeout: 60,
+		request: {
+			"credentials": [
+				{
+					"credential": "irma-demo.Surfnet.root",
+					"attributes": {
+						"userID": userID,
+						"securityHash": "00000000",
+					}
+				},
+				{
+					"credential": "irma-demo.MijnOverheid.fullName",
+					"attributes": {
+						"firstnames": firstnames,
+						"firstname": firstname,
+						"familyname": familyname,
+						"prefix": prefix,
+					}
+				}
+			],
+		}
+	};
+
+	var showWarning = function(msg) {
+		$("#alert_box").html('<div class="alert alert-warning" role="alert">'
+			 + '<strong>Warning:</strong> ' + msg + '</div>');
+	};
+
+	var showError = function (msg) {
+		$("#alert_box").html('<div class="alert alert-danger" role="alert">'
+			 + '<strong>Error:</strong> ' + msg + '</div>');
+	};
+
+	var success_fun = function(data) {
+		window.location = "issued.html";
+	};
+
+	$("#enroll").on("click", function() {
+		var jwt = IRMA.createUnsignedIssuanceJWT(iprequest);
+		IRMA.issue(jwt, success_fun, showWarning, showError);
+	});
+});
---
+++ js/verify.js
@@ -0,0 +1,49 @@
+$(function() {
+	// Setup verification request
+	var sprequest = {
+		"validity": 60,
+		"request": {
+			"content": [
+				{
+					"label": "Surfnet Root",
+					"attributes": ["irma-demo.Surfnet.root.userID"]
+				},
+			]
+		}
+	};
+
+	var success_fun = function(data) {
+		console.log("Authentication successful!");
+		console.log("Authentication token:", data);
+		$("#result_status").html("Success!");
+		$("#result_header").html("Result");
+		var json_token  = jwt_decode(data);
+		var attributes = json_token.attributes;
+		var surfnet_id = attributes['irma-demo.Surfnet.root.userID'];
+		json_string  = JSON.stringify(surfnet_id, null, 2);
+		$("#token-content").html("<b>irma-demo.Surfnet.root.userId </b>" + surfnet_id);
+	}
+
+	var cancel_fun = function(data) {
+		console.log("Authentication cancelled!");
+		$("#result_header").html("Result");
+		$("#result_status").html("Cancelled!");
+	}
+
+	var error_fun = function(data) {
+		console.log("Authentication failed!");
+		console.log("Error data:", data);
+		$("#result_header").html("Result");
+		$("#result_status").html("Failure!");
+	}
+
+	$("#verify_surfnet_root_btn").on("click", function() {
+		console.log("Button clicked");
+		$("#result_header").text("");
+		$("#result_status").text("");
+		$("#token-content").text("");
+		var jwt = IRMA.createUnsignedVerificationJWT(sprequest);
+		IRMA.verify(jwt, success_fun, error_fun);
+	});
+
+});
