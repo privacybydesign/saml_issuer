fed522860079947da763b2804c4c9b1ba2ef282a
Sietse Ringers <mail@sietseringers.net>
2017-03-21 09:08:03 +0100

    Deploy at deeper dir level

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ROOT_DIR', __DIR__ . '/../../');
+define('ROOT_DIR', __DIR__ . '/../../../');
 
 require_once ROOT_DIR . 'php/vendor/autoload.php';
 require_once ROOT_DIR . 'simplesamlphp/lib/_autoload.php';
