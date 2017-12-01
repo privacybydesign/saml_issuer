417420268aabe24df1df941152c935a41f604d3a
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-12-01 18:39:53 +0100

    Fix Surfnet (hopefully)

--- surfnet/index.php
+++ surfnet/index.php
@@ -13,13 +13,13 @@ define('IRMA_NAME_ATTRIBUTE', 'fullname');
 define('VERIFY_NAME_LABEL', 'Surfnet full name');
 
 define('MAP_IRMA_SAML_ATTRIBUTES', [
-    'institute'  => 'urn:mace:terena.org:attribute-def:schacHomeOrganization';
-    'type'       => 'urn:mace:dir:attribute-def:eduPersonAffiliation';
-    'id'         => 'urn:mace:dir:attribute-def:uid';
-    'fullname'   => 'urn:mace:dir:attribute-def:cn';
-    'firstname'  => 'urn:mace:dir:attribute-def:givenName';
-    'familyname' => 'urn:mace:dir:attribute-def:sn';
-    'email'      => 'urn:mace:dir:attribute-def:mail';
+    'institute'  => 'urn:mace:terena.org:attribute-def:schacHomeOrganization',
+    'type'       => 'urn:mace:dir:attribute-def:eduPersonAffiliation',
+    'id'         => 'urn:mace:dir:attribute-def:uid',
+    'fullname'   => 'urn:mace:dir:attribute-def:cn',
+    'firstname'  => 'urn:mace:dir:attribute-def:givenName',
+    'familyname' => 'urn:mace:dir:attribute-def:sn',
+    'email'      => 'urn:mace:dir:attribute-def:mail',
 ]);
 
 define('ATTRIBUTE_HUMAN_NAMES', [
