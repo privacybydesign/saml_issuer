b39e9a2da42916a8b6f319b461ba16b52284cc86
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-12-01 18:41:54 +0100

    Add LinkedIn

---
+++ linkedin/index.php
@@ -0,0 +1,24 @@
+<?php
+
+define('PROVIDER', 'linkedin');
+define('PROVIDER_NAME', 'LinkedIn');
+define('SERVER_NAME', 'linkedin_enroll');
+define('CREDENTIAL', 'pbdf.pbdf.linkedin');
+define('IRMA_NAME_ATTRIBUTE', 'fullname');
+define('VERIFY_NAME_LABEL', 'LinkedIn full name');
+
+define('MAP_IRMA_SAML_ATTRIBUTES', [
+    'id'          => 'linkedin.id',
+    'firstname'   => 'linkedin.firstName',
+    'familyname'  => 'linkedin.lastName',
+    'email'       => 'linkedin.emailAddress',
+]);
+
+define('ATTRIBUTE_HUMAN_NAMES', [
+    'id'          => 'ID',
+    'firstname'   => 'Voornaam',
+    'familyname'  => 'Achternaam',
+    'email'       => 'E-mailadres',
+]);
+
+require '../index.php';
