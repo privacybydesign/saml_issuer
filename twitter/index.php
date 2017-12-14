a631c619c9340b1304c08e5f6a70c770d3003c8a
Sietse Ringers <mail@sietseringers.net>
2017-12-14 11:16:09 +0100

    Remove ID attribute from Twitter credential

--- twitter/index.php
+++ twitter/index.php
@@ -9,7 +9,6 @@ define('PROFILE_URL_FORMAT', 'https://twitter.com/:username:');
 define('VERIFY_NAME_LABEL', 'Twitter full name');
 
 $MAP_IRMA_SAML_ATTRIBUTES = [
-    'id'          => 'twitter.id_str',
     'username'    => 'twitter.screen_name',
     'fullname'    => 'twitter.name',
     'email'       => 'twitter.email',
