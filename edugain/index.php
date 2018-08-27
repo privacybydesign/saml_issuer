4130a822a8ad1f18809cf3d16b0a31fae4d3d032
Sietse Ringers <mail@sietseringers.net>
2018-08-27 15:40:17 +0200

    Switch to oid's in edugain attribute mapping

--- edugain/index.php
+++ edugain/index.php
@@ -8,14 +8,14 @@ define('IRMA_NAME_ATTRIBUTE', 'fullname');
 define('VALIDITY', '+3 months');
 
 $MAP_IRMA_SAML_ATTRIBUTES = [
-    'institute'  => 'urn:mace:terena.org:attribute-def:schacHomeOrganization',
-    'type'       => 'urn:mace:dir:attribute-def:eduPersonAffiliation',
-    'id'         => 'urn:mace:dir:attribute-def:uid',
-    'fullid'     => 'urn:mace:dir:attribute-def:eduPersonPrincipalName',
-    'fullname'   => 'urn:mace:dir:attribute-def:displayName',
-    'firstname'  => 'urn:mace:dir:attribute-def:givenName',
-    'familyname' => 'urn:mace:dir:attribute-def:sn',
-    'email'      => 'urn:mace:dir:attribute-def:mail',
+    'institute'  => 'urn:oid:1.3.6.1.4.1.25178.1.2.9',
+    'type'       => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.1',
+    'id'         => 'urn:oid:0.9.2342.19200300.100.1.1',
+    'fullid'     => 'urn:oid:1.3.6.1.4.1.5923.1.1.1.6',
+    'fullname'   => 'urn:oid:2.16.840.1.113730.3.1.241',
+    'firstname'  => 'urn:oid:2.5.4.42',
+    'familyname' => 'urn:oid:2.5.4.4',
+    'email'      => 'urn:oid:0.9.2342.19200300.100.1.3',
 ];
 
 require '../index.php';
