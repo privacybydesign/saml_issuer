4c5c4378211686c781a0c4f5b66fd935ab744303
David Venhoek <david@venhoek.nl>
2019-11-05 17:14:20 +0100

    Fixed verification sessions.

--- config.sample.php
+++ config.sample.php
@@ -5,6 +5,7 @@ define('ROOT_DIR', __DIR__ . '/../../../');
 define('LOG_FILE', ROOT_DIR . 'logs/php.log');
 define('IRMA_SERVER_API_TOKEN', '');
 define('IRMA_SERVER_URL', 'http://localhost:8088');
+define('IRMA_SERVER_URL_BACKEND', 'http://localhost:8088');
 
 require_once ROOT_DIR . 'php/vendor/autoload.php';
 require_once ROOT_DIR . 'simplesamlphp/lib/_autoload.php';
