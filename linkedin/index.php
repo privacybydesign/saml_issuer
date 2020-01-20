b75c9f49abf8c4bb5829c75fe09d8bb563d63dbf
Ivar Derksen <ivarderksen@hotmail.nl>
2020-01-20 23:33:10 +0100

    Updated LinkedIn issuer to V2 API

--- linkedin/index.php
+++ linkedin/index.php
@@ -4,15 +4,13 @@ define('PROVIDER', 'linkedin');
 define('PROVIDER_NAME', 'LinkedIn');
 define('SERVER_NAME', 'linkedin_enroll');
 define('CREDENTIAL', 'pbdf.pbdf.linkedin');
-define('IRMA_NAME_ATTRIBUTE', 'fullname');
+define('IRMA_NAME_ATTRIBUTE', 'familyname');
 define('VALIDITY', '+1 year');
 
 $MAP_IRMA_SAML_ATTRIBUTES = [
-    'fullname'    => 'linkedin.formattedName',
     'firstname'   => 'linkedin.firstName',
     'familyname'  => 'linkedin.lastName',
     'email'       => 'linkedin.emailAddress',
-    'profileurl'  => 'linkedin.publicProfileUrl',
 ];
 
 require '../index.php';
