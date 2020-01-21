cae6c03334faea1d583919607cd9db255e23d72c
Ivar Derksen <ivarderksen@hotmail.nl>
2020-01-21 15:35:59 +0100

    Pin simplesamlphp to version indicated in fugakeyshare

--- install-simplesamlphp.sh
+++ install-simplesamlphp.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-wget -c https://github.com/simplesamlphp/simplesamlphp/releases/download/v1.18.3/simplesamlphp-1.18.3.tar.gz -O - | tar -xz
-mv simplesamlphp-1.18.3 simplesamlphp
+set -euxo pipefail
+
+wget -c https://github.com/simplesamlphp/simplesamlphp/releases/download/v1.17.7/simplesamlphp-1.17.7.tar.gz -O - | tar -xz
+mv simplesamlphp-1.17.7 simplesamlphp
 cd simplesamlphp
 composer require --no-update cirrusidentity/simplesamlphp-module-authoauth2 && composer update --no-dev cirrusidentity/simplesamlphp-module-authoauth2
