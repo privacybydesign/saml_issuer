85636faec8cc9348fe59fd2d79333ccd6e7860b0
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2024-11-20 16:35:50 +0100

    add config.php, remove config.sample.php
    - was in ignore, but is needed in docker container

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+define('LANG', 'en');
+
+define('ROOT_DIR', __DIR__ . '');
+define('LOG_FILE', ROOT_DIR . 'logs/php.log');
+define('IRMA_SERVER_API_TOKEN', getenv('API_TOKEN'));
+define('IRMA_SERVER_URL', getenv('IRMA_SERVER_URL'));
+define('IRMA_SERVER_URL_BACKEND', getenv('IRMA_SERVER_URL_BACKEND'));
+
+define('IRMA_ISSUER_ID', 'pbdf-staging.sidn-pbdf');
+
+require_once ROOT_DIR . '/vendor/autoload.php';
+require_once ROOT_DIR . '/simplesamlphp/lib/_autoload.php';
