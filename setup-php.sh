4259bac5900c202ff53882f03de2e71f331c35f6
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2024-11-26 09:23:00 +0100

    update simplesamlphp to 2.3.3

--- setup-php.sh
+++ setup-php.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-wget -c https://github.com/simplesamlphp/simplesamlphp/releases/download/v2.0.14/simplesamlphp-2.0.14.tar.gz -O - | tar -xz
-mv simplesamlphp-2.0.14 simplesamlphp
+wget -c https://github.com/simplesamlphp/simplesamlphp/releases/download/v2.3.3/simplesamlphp-2.3.3-full.tar.gz -O - | tar -xz
+mv simplesamlphp-2.3.3 simplesamlphp
 cd simplesamlphp
 composer require --no-update cirrusidentity/simplesamlphp-module-authoauth2 && composer update --no-dev cirrusidentity/simplesamlphp-module-authoauth2
 
