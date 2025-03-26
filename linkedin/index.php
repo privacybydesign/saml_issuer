3ee90bb5552d983acfe7788ead659653c87b764e
Martijn Kamphuis <m.kamphuis@caesar.nl>
2025-03-26 16:55:49 +0100

    Update LinkedIN OIDC mapping

--- linkedin/index.php
+++ linkedin/index.php
@@ -9,9 +9,9 @@ define('IRMA_NAME_ATTRIBUTE', 'name');
 define('VALIDITY', '+1 year');
 
 $MAP_IRMA_SAML_ATTRIBUTES = [
-    'firstname'   => 'given_name',
-    'familyname'  => 'family_name',
-    'email'       => 'email',
+    'firstname'   => 'oidc.given_name',
+    'familyname'  => 'oidc.family_name',
+    'email'       => 'oidc.email',
 ];
 
 require '../index.php';
