c6f435469b953af8ceb9480bacde828edd5197fa
Martijn Kamphuis <m.kamphuis@caesar.nl>
2025-04-01 14:21:35 +0200

    Fix PHP memory issue

--- run-on-start.sh
+++ run-on-start.sh
@@ -1,2 +1,5 @@
+# Set memory_limit in php.ini
+su -s "/bin/sh" -c "sed -i 's,^memory_limit =.*$,memory_limit = 256M,' /usr/local/etc/php/php.ini"
+
 # run a cron job in the background
 su -s "/bin/sh" -c "php -d max_execution_time=120 -d memory_limit=400M ${SSP_DIR}/modules/cron/bin/cron.php -t hourly" www-data
