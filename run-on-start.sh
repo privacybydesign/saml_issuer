ad2ea02e4a154544d87b6f8578922b7934c94056
Martijn Kamphuis <m.kamphuis@caesar.nl>
2025-04-02 17:46:58 +0200

    Start cron on background shell

--- run-on-start.sh
+++ run-on-start.sh
@@ -3,5 +3,5 @@ su -s "/bin/sh" -c "sed -i 's,^memory_limit =.*$,memory_limit = 256M,' /usr/loca
 
 # run a cron job in the background
 if [ -f "${SSP_DIR}/modules/cron/bin/cron.php" ]; then
-    su -s "/bin/sh" -c "php -d max_execution_time=120 -d memory_limit=400M ${SSP_DIR}/modules/cron/bin/cron.php -t hourly" www-data
+    su -s "/bin/sh" -c "php -d max_execution_time=120 -d memory_limit=400M ${SSP_DIR}/modules/cron/bin/cron.php -t hourly" www-data &
 fi
